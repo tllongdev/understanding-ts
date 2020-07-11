6739da280e3a38f387336427696cd3edca5c6263
Timothy Lee Long <tllongdev@gmail.com>
2020-07-11 13:43:59 -0500

    enums, added READ.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Understading TypeScript
+
+### review of the core concepts of TS
+
+#### scripts:
+
+- "start" runs lite-server on localhost:3000
+- (in a separate terminal) "mon" runs nodemon auto compile ts to js on save
--- app.ts
+++ app.ts
@@ -1,13 +1,32 @@
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string];
+// } = {
+// 	name: 'Maximilian',
+// 	age: 30,
+// 	hobbies: ['Sports', 'Cooking'],
+// 	role: [2, 'author'], //tuple
+// };
+
+enum Role {
+	ADMIN,
+	READ_ONLY,
+	AUTHOR,
+}
+
 const person: {
 	name: string;
 	age: number;
 	hobbies: string[];
-	role: [number, string];
+	role: any;
 } = {
 	name: 'Maximilian',
 	age: 30,
 	hobbies: ['Sports', 'Cooking'],
-	role: [2, 'author'], //tuple
+	// role: [2, 'author'], //tuple
+	role: Role.ADMIN, //enum
 };
 
 // person.role.push('admin') // watch out for .push - TS will not catch err on tuples
